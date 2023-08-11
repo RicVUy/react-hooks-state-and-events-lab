a712713ec382045cc26b0cff4dee2cbcb187e4a6
RicardoUy <rvuy62@yahoo.com>
2023-08-11 16:01:41 -0500

    submit assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [text, setText] = useState("Add to Cart");
+
+  function handleButtonText() {
+    setText(text === "Add to Cart" ? "Remove from Cart" : "Add to Cart");
+  }
+
+  const appClass = text === "Add to Cart" ? "" : "in-cart";
+
   return (
-    <li className="">
+    <li className={appClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleButtonText}>
+        {text}
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+const [selectedCategory, setCategory] = useState("All");
+
+function handleFilterChange(event) {
+  setCategory(event.target.value);
+}
+const itemsToDisplay = items.filter((item) => {
+  if (selectedCategory === "All" || selectedCategory === item.category) {
+    return item;
+  }
+  return null;
+});
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilterChange} >
           <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
+          <option value="Produce"> Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
